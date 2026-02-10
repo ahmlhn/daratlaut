223aab41fc143fa3f59ac6fea3f67ff3e7cd4d72
ahmlhn <ahmadsolihin969@gmail.com>
2026-02-10 13:24:47 +0700

    fix(auth): avoid 419 after logout relogin

---
+++ public/build/assets/SystemUpdate-CXdGYhRL.js
@@ -0,0 +1,1 @@
+import{c as t,a as e,u as a,h as r,m as s,F as m,j as _}from"./app-Dm87D0yl.js";import{_ as o}from"./AdminLayout-n9J4pJGO.js";import n from"./SystemUpdatePanel-U1IMwGaB.js";import"./LoadingSpinner-BBMKV3Ot.js";const h={__name:"SystemUpdate",setup(c){return(l,p)=>(_(),t(m,null,[e(a(r),{title:"Update Sistem"}),e(o,null,{default:s(()=>[e(n)]),_:1})],64))}};export{h as default};
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,8 +2,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
-const token = document.head.querySelector('meta[name="csrf-token"]');
-if (token && token.content) {
-    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
-}
+// Don't set a static X-CSRF-TOKEN header from the meta tag.
+// In an Inertia SPA, the initial Blade-rendered meta token won't refresh on client-side navigation,
+// which can cause 419 "Page Expired" after logout/login (session token regeneration).
+// Axios will automatically send the current token via the XSRF cookie (XSRF-TOKEN -> X-XSRF-TOKEN).
